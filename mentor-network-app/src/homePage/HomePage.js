7c28cbf05a1b813ce20d0b9d7779b1c3ea52688d
yash <yashmundra06@gmail.com>
2022-11-09 21:17:44 -0600

    Adding home page

---
+++ mentor-network-app/src/homePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HomePage(){
+    return (
+        <div className='HomePageContainer'>
+            <p>Logo</p>
+            <button>Login</button>
+            <p>Title</p>
+            <p>Title description</p>
+            <button>Search for Mentors</button>
+            <button>Become a mentor</button>
+            
+    </div>
+    )
+}
