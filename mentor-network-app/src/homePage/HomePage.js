1dab10978f4e51a17ad56a4ed7a47af8cbdf5c84
yash <yashmundra06@gmail.com>
2022-11-09 22:39:08 -0600

    Home page complete

--- mentor-network-app/src/homePage/HomePage.js
+++ mentor-network-app/src/homePage/HomePage.js
@@ -1,14 +1,17 @@
 import React from "react";
+import './HomePage.css'
 
 export default function HomePage(){
     return (
-        <div className='HomePageContainer'>
-            <p>Logo</p>
-            <button>Login</button>
-            <p>Title</p>
-            <p>Title description</p>
-            <button>Search for Mentors</button>
-            <button>Become a mentor</button>           
+        <div className='homePageContainer'>
+            <p className="logo">Mentor Network</p>
+            <button className="loginButton">SignUp/Login</button>
+            <p className="title">1-1 Mentoring</p>
+            <p className="titleDescription1">Master your craft with leading mentors at your side.<br></br> </p>
+            <p className="titleDescription2">Grow with every online mentoring session and take the next step in your career.<br></br> </p>
+            <p className="titleDescription3">All on your terms, for a flat monthly price </p>
+            <button className="searchMentorsButton">Search for Mentors</button>
+            <button className="becomeMentorButton">Become a mentor</button>           
     </div>
     )
 }
